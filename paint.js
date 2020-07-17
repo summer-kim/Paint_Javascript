adae84e61736925d05fcaed6a1c9415ed56b3f34
summer-kim <48466707+summer-kim@users.noreply.github.com>
2020-07-18 02:04:52 +0900

    add color change, brush size chane
    you have to check element's name
    with console.dir(something);
    before use

--- paint.js
+++ paint.js
@@ -1,26 +1,52 @@
 const canvas = document.getElementById("jsCanvas");
+const ctx = canvas.getContext("2d");
+const colors = document.getElementsByClassName("color");
+const range = document.getElementsByClassName("range")[0];
 
-let paint = False;
+let paint = false;
+
+ctx.strokeStyle = "#2c2c2c";
+ctx.lineWidth = 2.5;
 
 function stopPainting(event) {
-  paint = False;
+  paint = false;
 }
 function startPainting(event) {
-  paint = True;
+  paint = true;
 }
 
 function mouseMove(event) {
-  const x = event.offSetX;
-  const y = event.offSetY;
+  const x = event.offsetX;
+  const y = event.offsetY;
   if (!paint) {
-    context.beginPath();
+    ctx.beginPath();
+    ctx.moveTo(x, y);
   } else {
+    ctx.lineTo(x, y);
+    ctx.stroke();
   }
 }
 
+function handleColor(event) {
+  const color = event.target.style.backgroundColor;
+  ctx.strokeStyle = color;
+}
+function handleSize(event) {
+  const brushSize = event.target.value;
+  ctx.lineWidth = brushSize;
+}
+
 if (canvas) {
-  canvas.addEventListener("mouseenter", mouseMove);
+  canvas.addEventListener("mousemove", mouseMove);
   canvas.addEventListener("mousedown", startPainting);
   canvas.addEventListener("mouseleave", stopPainting);
   canvas.addEventListener("mouseup", stopPainting);
+}
+
+Array.from(colors).forEach((color) =>
+  color.addEventListener("click", handleColor)
+);
+
+if (range) {
+  range.addEventListener("input", handleSize);
 }
